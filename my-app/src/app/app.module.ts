a1b0fd02dc46b3cffdebd73fb711fc944e398136
ChristopheGueroult <christophegueroult@gmail.comm>
2018-06-25 14:32:57 +0200

    architecture modulaire

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule,
+    PageNotFoundModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
